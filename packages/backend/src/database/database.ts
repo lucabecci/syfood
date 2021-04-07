bc9be3aa251ee3330c4d6a35f5a8c4904a552f2d
luca <beccibrian@gmail.com>
2021-04-07 20:41:57 -0300

    chore(Backend): DB connection added

--- packages/backend/src/database/database.ts
+++ packages/backend/src/database/database.ts
@@ -1,3 +1,29 @@
+import mongoose, { ConnectOptions, Mongoose } from "mongoose";
 class Database {
+  private static _mongoose: Mongoose = mongoose;
+  private static _conf: ConnectOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  };
+
+  protected static async connect(): Promise<void> {
+      let retries = 5
+      while (retries) {
+        try{
+            await this._mongoose.connect("", this._conf)
+            console.log("DB is connected")
+            break;
+          }
+          catch(e){
+              console.log(e)
+              retries--
+              console.log(`Retries left: ${retries}`)
+              await new Promise((res) => setTimeout(res, 5000));
+          }
+      }
+  }
 
 }
+
+export default Database
