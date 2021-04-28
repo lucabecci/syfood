f58850903d7a1ee9ba6642812fccbeb6e9ed6595
lucabecci <beccibrian@gmail.com>
2021-04-28 00:27:04 -0300

    chore(Backend): IsLogged added

--- packages/backend/src/middlewares/IsLogged.ts
+++ packages/backend/src/middlewares/IsLogged.ts
@@ -1,3 +1,15 @@
-class IsLogged {}
+import { NextFunction, Request, Response } from "express";
+
+class IsLogged {
+    public static logged(req: Request, res: Response, next: NextFunction): void{
+        if( req.user ){
+            next()
+        } else {
+            res.status(401).json({
+                message: "Auth not valid"
+            })
+        }
+    }  
+}
 
 export default IsLogged
