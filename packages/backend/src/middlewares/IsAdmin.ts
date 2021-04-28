6d5cb6ff0315910f35797ea2994796397a335397
lucabecci <beccibrian@gmail.com>
2021-04-28 00:27:19 -0300

    chore(Backend): IsAdmin added

--- packages/backend/src/middlewares/IsAdmin.ts
+++ packages/backend/src/middlewares/IsAdmin.ts
@@ -1,3 +1,27 @@
-class IsAdmin {}
+import { getModelForClass } from "@typegoose/typegoose";
+import { NextFunction, Request, Response } from "express";
+import User from "src/models/User";
+
+class IsAdmin {
+    private static user = getModelForClass(User)
+
+    public static async isAdmin(req: Request, res: Response, next: NextFunction): Promise<void> {
+        if(req.user){
+            const usr = await this.user.findOne(req.user)
+
+            if(usr?.admin){
+                next()
+            }else{
+                res.status(401).json({
+                    message: "Not a admin"
+                })
+            }
+        }else{
+            res.status(401).json({
+                message: "Auth not valid"
+            })
+        }
+    }
+}
 
 export default IsAdmin
